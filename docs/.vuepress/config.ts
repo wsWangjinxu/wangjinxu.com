64884aac6da7ebfdce2911fcf9dc28432b7a4ca6
wsWangjinxu <1576520768@qq.com>
2022-10-15 15:06:33 +0800

    fix(config): base to root

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  base: '/wangjinxu.com/',
+  base: '/',
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
